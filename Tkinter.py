1f2e017df062fb7ec31d0d338379fbe796ec7526
Rolol0ko <107160374+Rolol0ko@users.noreply.github.com>
2022-09-13 20:53:45 -0400

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,12 +1,24 @@
 from tkinter import *
+from tkinter import messagebox
 import tkinter
 
+def ButtonPress():
+    print("button Pressed")
+    
 #set up window
 root = Tk()
 root.title("Tkinter test")
 root.geometry("910x540")
+root.resizable(False, False)
 
 Widget1 = tkinter.Label(root, text="Hello World")
+Widget1.pack(ipadx=10, ipady=10)
+
+Widget2 = tkinter.Button(root, text="Button", command=ButtonPress())
+Widget2.pack(ipadx=20, ipady=20, anchor=N) 
+
+Widget3 = tkinter.Label(root, text="This is supposed to display button presses")
+Widget3.pack(ipadx=10,ipady=10)
 
 #run the window
 root.mainloop()
