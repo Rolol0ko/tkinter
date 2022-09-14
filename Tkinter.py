6902f436546fc721d27d910aea850d50a1bf060e
Rolol0ko <107160374+Rolol0ko@users.noreply.github.com>
2022-09-13 20:27:46 -0400

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,3 +1,12 @@
 from tkinter import *
+import tkinter
+
+#set up window
 root = Tk()
+root.title("Tkinter test")
+root.geometry("910x540")
+
+Widget1 = tkinter.Label(root, text="Hello World")
+
+#run the window
 root.mainloop()
