f4e942f22866ad26ed2217aac4e447e13d2278ca
Rolol0ko <ronan.keown613@gmail.com>
2022-09-14 13:26:59 -0400

    More Click Work

--- Tkinter.py
+++ Tkinter.py
@@ -2,22 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 import tkinter
 
+#Variables
+clicks = int(0)
+
 def ButtonPress():
-    print("button Pressed")
-    
+    global clicks
+    clicks += 1
+    print("button Pressed " + str(clicks) + " Times")
+
 #set up window
 root = Tk()
-root.title("Tkinter test")
-root.geometry("910x540")
+root.title("Clicks")
+root.geometry("200x250")
 root.resizable(False, False)
 
+#Wiget set up
 Widget1 = tkinter.Label(root, text="Hello World")
 Widget1.pack(ipadx=10, ipady=10)
 
-Widget2 = tkinter.Button(root, text="Button", command=ButtonPress())
+Widget2 = tkinter.Button(root, text="Button", command=ButtonPress)
 Widget2.pack(ipadx=20, ipady=20, anchor=N) 
 
-Widget3 = tkinter.Label(root, text="This is supposed to display button presses")
+Widget3 = tkinter.Label(root, text="Button Was Clicked " + str(clicks) + " times")
 Widget3.pack(ipadx=10,ipady=10)
 
 #run the window
